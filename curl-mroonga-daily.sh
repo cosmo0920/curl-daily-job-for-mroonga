d6414913c2f3dfbb56413312cc7d1093355f44cc
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-08-09 03:15:53 +0900

    error handling

--- curl-mroonga-daily.sh
+++ curl-mroonga-daily.sh
@@ -5,4 +5,10 @@ export MROONGAVER="4.04"
 # e.g. # => 2014.07.01
 export DATE="`date +\"%Y.%m.%d\"`"
 # curl
-curl -O "http://packages.groonga.org/nightly/mariadb-${MARIADBVER}-with-mroonga-${MROONGAVER}-for-windows.${DATE}.zip"
+export URL="http://packages.groonga.org/nightly/mariadb-${MARIADBVER}-with-mroonga-${MROONGAVER}-for-windows.${DATE}.zip"
+STATUSCODE=$(curl --write-out "%{http_code}\n" --silent --output /dev/null "$URL")
+if [ $STATUSCODE -ne 200 ]; then
+    exit 1
+fi
+curl -O $URL
+echo $?
