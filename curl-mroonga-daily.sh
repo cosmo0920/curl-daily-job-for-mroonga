0a51a9bf38a5e4a8bae023bd4765084e46beaa73
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-08-09 15:52:31 +0900

    use --fail option

--- curl-mroonga-daily.sh
+++ curl-mroonga-daily.sh
@@ -5,9 +5,4 @@ export MROONGAVER="4.04"
 # e.g. # => 2014.07.01
 export DATE="`date +\"%Y.%m.%d\"`"
 # curl
-export URL="http://packages.groonga.org/nightly/mariadb-${MARIADBVER}-with-mroonga-${MROONGAVER}-for-windows.${DATE}.zip"
-STATUSCODE=$(curl --write-out "%{http_code}\n" --silent --output /dev/null "$URL")
-if [ $STATUSCODE -ne 200 ]; then
-    exit 1
-fi
-curl -O $URL
+curl --fail -O "http://packages.groonga.org/nightly/mariadb-${MARIADBVER}-with-mroonga-${MROONGAVER}-for-windows.${DATE}.zip"
