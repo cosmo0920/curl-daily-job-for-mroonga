cb840582ee6534f243275cfd5c631095d37a9559
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-08-29 22:50:58 +0900

    bump up Mroonga version
    4.04 ->
    4.05
       ^

--- curl-mroonga-daily.sh
+++ curl-mroonga-daily.sh
@@ -1,6 +1,6 @@
 # specify MariaDB and Mroonga version
 export MARIADBVER="10.0.13"
-export MROONGAVER="4.04"
+export MROONGAVER="4.05"
 # get Date
 # e.g. # => 2014.07.01
 export DATE="`date +\"%Y.%m.%d\"`"
