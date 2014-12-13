5e37ce87de4e6c1fb5b97c261e2d44eaddc5da1d
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-12-13 17:04:13 +0900

    add `--no-run-if-empty` when using Linux

--- curl-mroonga-daily.sh
+++ curl-mroonga-daily.sh
@@ -5,8 +5,13 @@ export MROONGAVER="4.08"
 # e.g. # => 2014.07.01
 export DATE="`date +\"%Y.%m.%d\"`"
 export keep_n_days=30
+#xarg option
+XARGS_NO_RUN=''
+if [ `uname` = 'Linux' ]; then
+    XARGS_NO_RUN='--no-run-if-empty';
+fi
 # curl
 curl --fail -O "http://packages.groonga.org/nightly/mariadb-${MARIADBVER}-with-mroonga-${MROONGAVER}-for-windows.${DATE}.zip"
 
 find ./ -maxdepth 1 -type f -ctime +${keep_n_days} -print0 | \
-  xargs --null --no-run-if-empty rm
+  xargs --null $XARGS_NO_RUN rm
