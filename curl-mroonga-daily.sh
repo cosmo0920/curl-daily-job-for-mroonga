0b508b073f673e71bc680fe17c4ee7e5a6ca54b5
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-12-13 16:58:18 +0900

    remove over 30 days old nightly package(s)

--- curl-mroonga-daily.sh
+++ curl-mroonga-daily.sh
@@ -4,5 +4,9 @@ export MROONGAVER="4.08"
 # get Date
 # e.g. # => 2014.07.01
 export DATE="`date +\"%Y.%m.%d\"`"
+export keep_n_days=30
 # curl
 curl --fail -O "http://packages.groonga.org/nightly/mariadb-${MARIADBVER}-with-mroonga-${MROONGAVER}-for-windows.${DATE}.zip"
+
+find ./ -maxdepth 1 -type f -ctime +${keep_n_days} -print0 | \
+  xargs --null --no-run-if-empty rm
