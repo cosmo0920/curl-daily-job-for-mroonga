2cddb7ed786cd35b989d718cda89852e089d2e5b
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-12-20 16:47:58 +0900

    remove older package before curl task

--- curl-mroonga-daily.sh
+++ curl-mroonga-daily.sh
@@ -10,8 +10,9 @@ XARGS_NO_RUN=''
 if [ `uname` = 'Linux' ]; then
     XARGS_NO_RUN='--no-run-if-empty';
 fi
+# remove older than 30 days nightly package
+find ./*.zip -maxdepth 1 -type f -ctime +${keep_n_days} | \
+  xargs $XARGS_NO_RUN rm
 # curl
 curl --fail -O "http://packages.groonga.org/nightly/mariadb-${MARIADBVER}-with-mroonga-${MROONGAVER}-for-windows.${DATE}.zip"
 
-find ./*.zip -maxdepth 1 -type f -ctime +${keep_n_days} | \
-  xargs $XARGS_NO_RUN rm
