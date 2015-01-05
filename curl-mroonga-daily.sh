8d29442eec642d7c7bae1c013803ed5050612cb7
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2015-01-05 19:55:03 +0900

    Update Mroonga version

--- curl-mroonga-daily.sh
+++ curl-mroonga-daily.sh
@@ -1,6 +1,6 @@
 # specify MariaDB and Mroonga version
 export MARIADBVER="10.0.15"
-export MROONGAVER="4.08"
+export MROONGAVER="4.09"
 # get Date
 # e.g. # => 2014.07.01
 export DATE="`date +\"%Y.%m.%d\"`"
