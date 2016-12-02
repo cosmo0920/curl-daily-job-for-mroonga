d860ddb60933b737ed0af7eaeee5869d3c1d3b04
Hiroshi Hatake <hatake@clear-code.com>
2016-12-02 17:37:06 +0900

    Bump up Mroonga version to 6.12

--- curl-mroonga-daily.sh
+++ curl-mroonga-daily.sh
@@ -1,7 +1,7 @@
 # specify MariaDB and Mroonga version
 set -x
 export MARIADBVER="10.1.19"
-export MROONGAVER="6.11"
+export MROONGAVER="6.12"
 # get Date
 # e.g. # => 2014.07.01
 export DATE="`date +\"%Y.%m.%d\"`"
